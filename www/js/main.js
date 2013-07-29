43f0be5926e357343aad59fffdc8d6310b981192
Rylan Bowers <rylanb@gmail.com>
2013-07-28 21:14:02 -0600

    Setting up basic JS framework with initializer function for the home/index page.

--- www/js/main.js
+++ www/js/main.js
@@ -1,1 +1,13 @@
+(function($, window) {
 
+  var POLAROID = window.POLAROID || {}
+
+  POLAROID.Home = {
+    init: function() {
+    }
+  }
+
+  window.POLAROID = POLAROID;
+  $(document).ready(POLAROID.Home.init());
+
+})(jQuery, this);
