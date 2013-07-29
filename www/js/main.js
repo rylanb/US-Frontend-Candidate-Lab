04b557f586d666cc23fe696e07adad909b163740
Rylan Bowers <rylanb@gmail.com>
2013-07-28 23:24:58 -0600

    Adding thanks-notice and javascript click handlers to handle yes/no clicks and display the thank you for responding message.

--- www/js/main.js
+++ www/js/main.js
@@ -4,6 +4,29 @@
 
   POLAROID.Home = {
     init: function() {
+      this.setupClickHandlers();
+    },
+    setupClickHandlers: function() {
+      $('#invitation').on('click', 'a', function(e) {
+        POLAROID.Home.handleInviteClicks(e);
+      });
+    },
+    handleInviteClicks: function(e) {
+      e.preventDefault();
+      var answer = $(e.target).data('answer');
+      answer === 'yes' ? this.inviteYesClickHandler() : this.inviteNoClickHandler();
+
+      $('#answer-buttons').fadeOut(function(){
+        $('#thanks-notice').fadeIn('fast', function(){
+          answer === 'yes' ? $(this).find('p').first().fadeIn() : $(this).find('p.no-msg').fadeIn();
+        });
+      });
+    },
+    inviteYesClickHandler: function() {
+      //Potential handling here if needed. Defaults are for yes in html/styles
+    },
+    inviteNoClickHandler: function() {
+      $('#thanks-notice').addClass('answered-no');
     }
   }
 
